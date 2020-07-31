4d66d94beedaa1cc6b82c1df9b05d24a18e3e84f
Goncalves Antoine <antoine.goncalves-isc.france@ibm.com>
2020-07-31 11:12:53 +0200

    Data Validation and Sanitization: Part I

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -9,24 +10,43 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    },
   },
 });
 
-// const me = new User({
-//   name: 'Antoine',
-//   age: 23,
-// });
+const me = new User({
+  name: ' Antoine  ',
+  email: 'ANTOINE.goncalves1410@gmail.COM   ',
+});
 
-// me.save()
-//   .then((me) => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log('Error!', error);
-//   });
+me.save()
+  .then((me) => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log('Error!', error);
+  });
 
 const Task = mongoose.model('Task', {
   description: {
@@ -37,16 +57,16 @@ const Task = mongoose.model('Task', {
   },
 });
 
-const task = new Task({
-  description: 'Clean home',
-  completed: false,
-});
+// const task = new Task({
+//   description: 'Clean home',
+//   completed: false,
+// });
 
-task
-  .save()
-  .then((task) => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// task
+//   .save()
+//   .then((task) => {
+//     console.log(task);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
