987c08bfb309c089bdc26006ff355cc21d1882fa
Gonçalves Antoine <antoine.goncalves-isc.france@ibm.com>
2021-05-20 10:10:07 +0200

    Refact routers user file

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -7,38 +8,57 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/login', async (req, res) => {
     try {
-        const users = await User.find({});
-        res.send(users);
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+
+        res.send({ user, token });
     } catch (e) {
-        res.status(500).send(e);
+        res.status(400).send();
     }
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        const user = await User.findById(_id);
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        });
 
-        if (!user) {
-        return res.status(404).send();
-    }
+        await req.user.save();
 
-    res.send(user);
+        res.send();
     } catch (e) {
-        res.status(500).send(e);
+        res.status(500).send();
     }
 });
 
-router.put('/users/:id', async (req, res) => {
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.get('/users/me', auth , async (req, res) => {
+    res.send(req.user);
+});
+
+
+
+router.put('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every((update) =>
@@ -50,30 +70,21 @@ router.put('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
+        updates.forEach((update) => req.user[update] = req.body[update]);
 
-        if (!user) {
-            return res.status(404).send();
-        }
+        await req.user.save();
 
-        res.send(user);
+        res.send(req.user);
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
+        await req.user.remove();
 
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
+        res.send(req.user);
     } catch (e) {
         res.status(500).send();
     }
